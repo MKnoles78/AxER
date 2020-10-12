b525f600e99b6657a1a01bf6851e3b324ac62192
MKnoles <mknoles78@gmail.com>
2020-10-11 21:50:45 -0400

    added two more containers

---
+++ src/containers/Drips.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Drips extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Drips;
