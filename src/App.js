02f57a264595f1654c589b6c64f86ebe6daff6a3
MKnoles <mknoles78@gmail.com>
2020-10-26 22:54:12 -0400

    added contact to nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Jumbotron from './components/Jumbotron';
 import Home from './containers/Home';
+import Contacts from './containers/Contacts'
 import './style.css'
 
 const App = () => {
@@ -13,6 +14,9 @@ const App = () => {
                 <Switch>
                     <Route exact path="/">
                         <Home />
+                    </Route>
+                    <Route exact path="/contacts">
+                        <Contacts />
                     </Route>
                 </Switch>
                 
