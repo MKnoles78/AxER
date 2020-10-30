62ef2bf5c807869a5de3d94edb8e1e3038e9afc2
MKnoles <mknoles78@gmail.com>
2020-10-29 22:27:15 -0400

    added main

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from './containers/Home';
 import Drips from './containers/Drips';
+import Main from './containers/Main';
 import Contacts from './containers/Contacts'
 import './style.css'
 
@@ -18,8 +19,11 @@ const App = () => {
                     <Route exact path="/contacts">
                         <Contacts />
                     </Route>
-                    <Route exact path="/contacts">
+                    <Route exact path="/drips">
                         <Drips />
+                    </Route>
+                    <Route exact path="/main">
+                        <Main />
                     </Route>
                 </Switch>
                 
